b0ecfd990ea575f5d9bee3efa676b89d1a870b30
Andrew Whatson <whatson@gmail.com>
2019-03-26 09:20:26 +1000

    WIP screencast utils, counsel fix merged

--- packages.el
+++ packages.el
@@ -1,23 +1,19 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;; HACK counsel fork, pending PR
-;; https://github.com/abo-abo/swiper/pull/1959
-(package! counsel
-  :recipe (:fetcher github :repo "flatwhatson/swiper"))
-
-;; HACK expand-region fork, upstream closed for PRs
-(package! expand-region
-  :recipe (:fetcher github :repo "flatwhatson/expand-region.el"))
+;; HACK expand-region-subword, waiting for MELPA listing
+;; https://github.com/melpa/melpa/pull/6087
 (package! expand-region-subword
   :recipe (:fetcher github :repo "flatwhatson/expand-region-subword"))
 
-;; HACK multiple-cursors fork, upstream closed for PRs
-(package! multiple-cursors
-  :recipe (:fetcher github :repo "flatwhatson/multiple-cursors.el"))
+;; HACK keycast fork, WIP doom compatibility
+(package! keycast
+  :recipe (:fetcher github :repo "flatwhatson/keycast"))
 
 (package! adaptive-wrap)
-(package! flycheck-package)
 (package! google-c-style)
 (package! goto-line-preview)
 (package! pkgbuild-mode)
+
+(package! flycheck-package)
+(package! gif-screencast)
