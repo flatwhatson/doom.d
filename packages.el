bbf3ec3566eaf99967bcb6b103e3bfe9a57e023c
Andrew Whatson <whatson@gmail.com>
2019-03-27 01:44:44 +1000

    Fork expand-region for org-mode unfold fix

--- packages.el
+++ packages.el
@@ -1,6 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
+;; HACK expand-region fork, fix org-mode unfolding
+;; https://github.com/flatwhatson/expand-region.el/commit/a667a126dccdb60a7d00555f05dff11395279ac2
+;; https://github.com/magnars/expand-region.el/commit/1f153a89a188013ac64586988b46418a5485b541#comments
+(package! expand-region
+  :recipe (:fetcher github :repo "flatwhatson/expand-region"))
+
 ;; HACK expand-region-subword, waiting for MELPA listing
 ;; https://github.com/melpa/melpa/pull/6087
 (package! expand-region-subword
