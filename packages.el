ce32762dbc8eb7cf389ec98a2ce371a674c254d4
Andrew Whatson <whatson@gmail.com>
2019-03-28 13:28:44 +1000

    Use expand-region from MELPA

--- packages.el
+++ packages.el
@@ -1,10 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;; HACK expand-region fork, waiting for MELPA update
-(package! expand-region
-  :recipe (:fetcher github :repo "magnars/expand-region.el"))
-
 ;; HACK keycast fork, WIP doom compatibility
 (package! keycast
   :recipe (:fetcher github :repo "flatwhatson/keycast"))
