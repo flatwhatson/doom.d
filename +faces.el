27b6f7528ae248b52633879c30502f20ca1a2df1
Andrew Whatson <whatson@gmail.com>
2020-05-29 19:14:51 +1000

    Remove box around demangled identifiers

--- +faces.el
+++ +faces.el
@@ -3,4 +3,5 @@
 (custom-set-faces!
  '((lsp-face-highlight-read lsp-face-highlight-write lsp-face-highlight-textual)
    :foreground nil :distant-foreground nil :background nil :weight bold :underline nil)
- '(ivy-minibuffer-match-face-2 :background nil))
+ '(ivy-minibuffer-match-face-2 :background nil)
+ '(demangled :box nil))
