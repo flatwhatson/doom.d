5419f973472ad37664c091738c4c2783955353f9
Andrew Whatson <whatson@gmail.com>
2020-06-04 19:52:09 +1000

    Add workaround for pdf-tools bug

--- packages.el
+++ packages.el
@@ -12,3 +12,8 @@
 
 ;; HACK use clangd instead of ccls
 (package! ccls :disable t)
+
+;; HACK https://github.com/politza/pdf-tools/pull/588
+(package! pdf-tools
+  :recipe (:host github :repo "flatwhatson/pdf-tools" :branch "fix-macros")
+  :pin "4a10c35e626ab6d7970dfd7335f4f883b1b879ef")
