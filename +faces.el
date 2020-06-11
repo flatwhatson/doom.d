0db93332f0597a6136f8c90eb32b7ba2f720dd85
Andrew Whatson <whatson@gmail.com>
2020-06-11 13:33:52 +1000

    Reindent faces and add some more overrides

--- +faces.el
+++ +faces.el
@@ -1,7 +1,9 @@
 ;;; ~/.doom.d/+faces.el -*- lexical-binding: t; -*-
 
 (custom-set-faces!
- '((lsp-face-highlight-read lsp-face-highlight-write lsp-face-highlight-textual)
-   :foreground nil :distant-foreground nil :background nil :weight bold :underline nil)
- '(ivy-minibuffer-match-face-2 :background nil)
- '(demangled :box nil))
+  '((lsp-face-highlight-read lsp-face-highlight-write lsp-face-highlight-textual)
+    :foreground nil :distant-foreground nil :background nil :weight bold :underline nil)
+  '(ivy-minibuffer-match-face-2 :background nil)
+  '(demangled :box nil)
+  '(info-menu-star :foreground nil)
+  `(link-visited :foreground ,(doom-color 'dark-blue)))
