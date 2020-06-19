4591fa59420778d8f8d92da07ae1690e80a600a1
Andrew Whatson <whatson@gmail.com>
2020-06-19 10:18:15 +1000

    Restore ccls, still the best

--- packages.el
+++ packages.el
@@ -10,9 +10,6 @@
 (package! google-c-style
   :recipe (:host github :repo "google/styleguide" :branch "gh-pages"))
 
-;; HACK use clangd instead of ccls
-(package! ccls :disable t)
-
 ;; HACK https://github.com/politza/pdf-tools/pull/588
 (package! pdf-tools
   :recipe (:host github :repo "flatwhatson/pdf-tools" :branch "fix-macros")
