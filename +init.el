0793e029cd512a277afdc180575237d1e6b17084
Andrew Whatson <whatson@gmail.com>
2020-09-01 11:44:27 +1000

    Use all cores for native compilation

--- +init.el
+++ +init.el
@@ -1,3 +1,6 @@
 ;;; ~/.doom.d/+init.el -*- lexical-binding: t; -*-
 
 ;; It is pitch black. You are likely to be eaten by a grue.
+
+(setq comp-async-jobs-number (string-to-number
+                              (shell-command-to-string "nproc")))
