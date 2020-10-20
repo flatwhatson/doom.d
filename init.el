4a16ab95fd5a715866381a2a8dad3cdc287c2d23
Andrew Whatson <whatson@gmail.com>
2020-10-20 13:28:18 +1000

    Rebase init

--- init.el
+++ init.el
@@ -116,7 +116,7 @@
        ;;(go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
-       ;;idris             ;
+       ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
        (java +lsp)       ; the poster child for carpal tunnel syndrome
        (javascript +lsp) ; all(hope(abandon(ye(who(enter(here))))))
