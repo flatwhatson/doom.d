d079597ae549c54c1698bba270f43266b3ef96cb
Andrew Whatson <whatson@gmail.com>
2022-06-23 10:28:38 +1000

    Remove redundant setting of comp-async-jobs-number

--- +init.el
+++ +init.el
@@ -1,6 +1,3 @@
 ;;; ~/.doom.d/+init.el -*- lexical-binding: t; -*-
 
 ;; It is pitch black. You are likely to be eaten by a grue.
-
-(setq comp-async-jobs-number (string-to-number
-                              (shell-command-to-string "nproc")))
